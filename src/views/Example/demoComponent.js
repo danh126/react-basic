9742d9b4d66d1233f1ea7c20880b9d40fa442c5c
danh126 <=>
2024-07-24 16:05:09 +0700

    state react

--- src/views/Example/demoComponent.js
+++ src/views/Example/demoComponent.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-//Tạo class Component
+/*
+Tạo class Component
+
+JSX => return về 1 khối lớp ngoài cùng
+
+Fragment để bao 1 khối (<> </>)
+
+className => đặt class cho thẻ html trong ReactJs
+*/
+
 class DemoComponent extends React.Component {
+
+    //state là 1 object
+    state = {
+        name: 'DANH NGUYEN',
+        home: 'Can Giuoc'
+    }
+
     render() {
-        return <div>Demo Component NTD</div>
+        // let name = 'DANH NGUYEN';
+        return (
+            <>
+                <div className="myname"> My Name is {this.state.name} </div>
+                <div className="test"> Live in {this.state['home']}</div >
+            </>
+        )
     }
 }
 
