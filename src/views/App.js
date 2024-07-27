df21b6e6160bc674d2fdcfd1cd166e8543334baf
danh126 <=>
2024-07-27 19:22:43 +0700

    html form with ReactJs

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.scss';
 import DemoComponent from './Example/demoComponent';
+import FormComponent from './Example/FormComponent';
 
 /**
  * có 2 kiểu components: class component / function component (function, arrow)
@@ -15,18 +16,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World With React Js (NTD)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <DemoComponent></DemoComponent>
+        <FormComponent />
       </header>
     </div>
   );
---
+++ src/views/Example/FormComponent.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class FormComponent extends React.Component {
+    state = {
+        firstName: '',
+        lastName: ''
+    }
+
+    handleFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault() //ngăn sự kiện load lại trang khi submit
+        console.log('>>> check data input:', this.state)
+    }
+
+    render() {
+        return (
+            <>
+                <form>
+                    <label>First Name</label><br />
+                    <input type="text" onChange={(event) => this.handleFirstName(event)} value={this.state.firstName} /><br />
+                    <label>Last Name</label><br />
+                    <input type="text" onChange={(event) => this.handleLastName(event)} value={this.state.lastName} /><br />
+                    <input type="submit" onClick={(event) => this.handleSubmit(event)} />
+                </form>
+            </>
+        )
+    }
+}
+
+export default FormComponent;
