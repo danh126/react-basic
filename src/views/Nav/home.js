65211fc8cb1b23f423ad619c2d0c167d2fabc046
danh126 <=>
2024-07-31 14:59:01 +0700

    Redirect

--- src/views/Nav/home.js
+++ src/views/Nav/home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 class Home extends React.Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo'); //thay đổi lịch sử chuyển đến trang /todo
+        }, 3000);
+    }
+
     render() {
+        console.log('>>> check props: ', this.props);
         return (
             <div>Hello World With Reactjs (NTD)</div>
         )
     }
 }
-export default Home;
+export default withRouter(Home);
